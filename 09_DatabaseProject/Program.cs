a0f061d81c7fc937bda9133b80f0cf7d763fc89a
Edanur AK <akeda7999@gmail.com>
2024-12-23 01:45:57 +0300

    Complete exercises for lessons 6-10

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado.net
+
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.WriteLine("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            Console.WriteLine("--------------------------");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("-----------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-59QRLF1; initial" +
+                " Catalog=EgitimKampiDb; integrated security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);//C# tarafında oluşturmuş olduğum sorgumla sql veri tabanı arasında bir köprü görevi görecek olan sınıf 
+            DataTable dataTable = new DataTable();//verilerimi geçiçi olarak ram belleğe almamı sağlayacak
+            adapter.Fill(dataTable);
+            connection.Close();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
